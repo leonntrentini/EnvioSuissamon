7f4f585e6d45e40aaf3071b4a7fbf1fa012c3367
Leonan <leonan.trentini@hotmail.com>
2016-08-15 00:53:21 -0300

    É o que tem pra hoje, funcionando meia boca.

---
+++ modules/Pokemon/model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+        const Schema = mongoose.Schema
+        const _schema = {
+        name: String,
+                attack: Number,
+                defense: Number
+                }
+
+const pokemonSchema = new Schema(_schema)
+        module.exports = mongoose.model('Pokemon', pokemonSchema)
